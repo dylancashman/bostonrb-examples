b5cbc5a1b5b568f2a815cc528c0671b280170185
Dylan Cashman <dylan.cashman@annkissam.com>
2012-05-28 16:20:46 -0400

    ran rails app generator

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BostonrbExamples::Application.config.secret_token = '83559c9a9cc803e3ba6dced96a9061b8fed2a01bd98a2b1f06d940b9d8ed912148f7908cd983a677bbf70f27ae0a866a0161b4fe10abb64a9110a1e7fff98c73'
